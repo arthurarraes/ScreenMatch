70c42e59925d5fcc3fe87a751962f43b1ff642d4
Arthur Arraes <109257634+arthurarraes@users.noreply.github.com>
2025-02-06 11:16:42 -0300

    criação do model e service

--- src/main/java/alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,13 +1,26 @@
 package alura.ScreenMatch;
 
+import alura.ScreenMatch.model.DadosSerie;
+import alura.ScreenMatch.service.ConsumoApi;
+import alura.ScreenMatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatchApplication {
+public class ScreenMatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println((dados));
+	}
 }
---
+++ src/main/java/alura/ScreenMatch/service/IConverteDados.java
@@ -0,0 +1,5 @@
+package alura.ScreenMatch.service;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+}
