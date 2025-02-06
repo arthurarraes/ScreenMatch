80e276c30ae8cfec6b5e40e508379c8417eb0c75
Arthur Arraes <109257634+arthurarraes@users.noreply.github.com>
2025-02-06 14:22:30 -0300

    Criação de Principal e de Model

--- src/main/java/alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,11 +1,13 @@
 package alura.ScreenMatch;
 
-import alura.ScreenMatch.model.DadosSerie;
-import alura.ScreenMatch.service.ConsumoApi;
-import alura.ScreenMatch.service.ConverteDados;
+import alura.ScreenMatch.model.DadosTemporada;
+import alura.ScreenMatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
@@ -16,11 +18,6 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println((dados));
-	}
-}
+		Principal principal = new Principal();
+		principal.exibeMenu();
+}}
